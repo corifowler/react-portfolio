a911039c67d5023bd542bdffbcd232a6d7527421
Cori Fowler <fowler.cori@gmail.com>
2018-08-13 21:42:52 -0400

    added font (raleway) + updated file structure a bit

---
+++ src/app/components/toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Toolbar.css";
+
+class Toolbar extends Component {
+    render() {
+        return (
+            <div className="Toolbar">
+                <div>This is the toolbar component!</div>
+            </div>
+        );
+    }
+}
+
+export default Toolbar;
